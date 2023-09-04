60f660a18d8f7cdf2b0dd03be536d3ad7736ddfe
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-09-04 21:40:41 +0200

    tasks 8-9 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginForm = document.querySelector('.login-form')
 
+console.log(loginForm)
+
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email === "" || password === "") {
+        alert("Всі поля повинні бути заповнені!🤷‍♂️")
+    } else {
+        const formObjectToSend = {
+            email: email,
+            password: password,
+        }
+        console.log(formObjectToSend)
+        form.reset();
+    }
+
+
+    
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const bodySelect = document.querySelector('body')
+
+const buttonChangeColor = bodySelect.children[1].children[1]
+const textCheckColor = bodySelect.children[1] .children[0].lastChild
+
+buttonChangeColor.addEventListener("click", () => {
+  const newBackColor = getRandomHexColor();
+  textCheckColor.textContent = newBackColor;
+  bodySelect.style.backgroundColor = newBackColor;
+})
