41d3eb686c60bee3bd5334fab5321d11c981a602
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-09-05 11:24:48 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const bodySelect = document.querySelector('body');
+
+const containerBox = bodySelect.children[2];
+const inputController = bodySelect.children[1].children[0];
+const buttonCreate = bodySelect.children[1].children[1];
+const buttonDestroy = bodySelect.children[1].children[2];
+
+buttonCreate.addEventListener("click", () => {
+  const quantityBoxes = Number(inputController.value);
+  let stringToPush = ''
+
+  for (let i = 0; i < quantityBoxes; i += 1){
+    const color = getRandomHexColor();
+    const boxSize = 30 + 10 * i;
+    
+    stringToPush += `<div style = "width: ${boxSize}px; height: ${boxSize}px; background-color:${color};"></div>`
+  }
+  containerBox.innerHTML = stringToPush
+})
+
+
+
+buttonDestroy.addEventListener("click", () => {
+  containerBox.innerHTML = "";
+})
